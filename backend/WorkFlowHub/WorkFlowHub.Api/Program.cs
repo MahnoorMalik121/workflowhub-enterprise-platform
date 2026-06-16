d0a6b884d51ea477bd74dde6eb68aeebe29e052c
Mahnoor Malik <mahnoorm2020@gmail.com>
2026-06-16 01:13:10 -0700

    Add department CRUD APIs

--- backend/WorkFlowHub/WorkFlowHub.Api/Program.cs
+++ backend/WorkFlowHub/WorkFlowHub.Api/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WorkFlowHub.Api.Data;
-
+using WorkFlowHub.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -10,7 +10,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<DepartmentService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
