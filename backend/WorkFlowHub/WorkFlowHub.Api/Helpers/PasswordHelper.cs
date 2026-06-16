c063cf6fb3fc08a05c21fc294fdc9bb456be9337
Mahnoor Malik <mahnoorm2020@gmail.com>
2026-06-16 03:27:36 -0700

    Add employee crud operations

--- backend/WorkFlowHub/WorkFlowHub.Api/Helpers/PasswordHelper.cs
+++ backend/WorkFlowHub/WorkFlowHub.Api/Helpers/PasswordHelper.cs
@@ -1,6 +1,25 @@
-﻿namespace WorkFlowHub.Api.Helpers
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WorkFlowHub.Api.Helpers
 {
-    public class PasswordHelper
+    public static class PasswordHelper
     {
+        public static string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var hashBytes = sha256.ComputeHash(passwordBytes);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static bool VerifyPassword(string password, string passwordHash)
+        {
+            var hashedPassword = HashPassword(password);
+
+            return hashedPassword == passwordHash;
+        }
     }
 }
--- backend/WorkFlowHub/WorkFlowHub.Api/Program.cs
+++ backend/WorkFlowHub/WorkFlowHub.Api/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<EmployeeService>(); 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
